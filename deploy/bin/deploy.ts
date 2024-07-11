304e88b526d16341df732eaec34d4f5938388a91
Carlos Chacon <cc.ib.chacon@gmail.com>
2024-07-11 00:28:09 -0400

    fix: correcion referencia ciclica
    se elimina stack para bucket y waf
    
    se agregan dentro del mismo stack para cloudfront

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -4,8 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { SSPApigatewayStack } from '../lib/apigateway/apigateway';
 import { SSPCloudfrontStack } from '../lib/cloudfront/ssp-cloudfront';
 import { SSPSupabaseSecretStack } from '../lib/secrets/spp-sercrets';
-import { SSPBucketStack } from '../lib/s3/ssp-bucket';
-import { SSPWafStack } from '../lib/waf/spp-waf';
 import { SSPRolesStack } from '../lib/roles/ssp-roles';
 import { Tags } from 'aws-cdk-lib';
 
@@ -26,17 +24,7 @@ var secretsmanager = new SSPSupabaseSecretStack(app, "SSPSupabaseSecretStack", {
     env: env
 });
 
-var bucket = new SSPBucketStack(app, "SSPBucketStack", {
-    env: env
-});
-
-var waf = new SSPWafStack(app, "SSPWafStack", {
-    env: env
-});
-
 var cloudfront = new SSPCloudfrontStack(app, "SSPCloudFrontStack", {
-    bucket: bucket.bucket,
-    waf: waf.webAcl,
     env: env
 });
 
@@ -47,6 +35,5 @@ var role = new SSPRolesStack(app, "SSPRolesStack", {
 
 Tags.of(apigateway).add('environment', process.env.ENVIRONMENT!);
 Tags.of(secretsmanager).add('environment', process.env.ENVIRONMENT!);
-Tags.of(bucket).add('environment', process.env.ENVIRONMENT!);
 Tags.of(cloudfront).add('environment', process.env.ENVIRONMENT!);
 Tags.of(role).add('environment', process.env.ENVIRONMENT!);
--- deploy/lib/cloudfront/ssp-cloudfront.ts
+++ deploy/lib/cloudfront/ssp-cloudfront.ts
@@ -1,20 +1,51 @@
 //add certificate manager
 
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { IBucket } from 'aws-cdk-lib/aws-s3';
+import { Bucket, BucketEncryption, IBucket } from 'aws-cdk-lib/aws-s3';
 import { CloudFrontAllowedCachedMethods, CloudFrontWebDistribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
 import { CfnWebACL } from 'aws-cdk-lib/aws-wafv2';
 
-interface SSPCloudfrontStackProps extends StackProps {
-    bucket: IBucket;
-    waf: CfnWebACL;
-}
 export class SSPCloudfrontStack extends Stack {
 
-    constructor(scope: Construct, id: string, props?: SSPCloudfrontStackProps) {
+    constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        const webAcl = new CfnWebACL(this, 'SSPWebACLId', {
+            defaultAction: { allow: {} },
+            scope: 'REGIONAL',
+            visibilityConfig: {
+                cloudWatchMetricsEnabled: true,
+                metricName: 'SSPWebACL',
+                sampledRequestsEnabled: true,
+            },
+            rules: [
+                {
+                    name: 'AWS-AWSManagedRulesCommonRuleSet',
+                    priority: 1,
+                    statement: {
+                        managedRuleGroupStatement: {
+                            vendorName: 'AWS',
+                            name: 'AWSManagedRulesCommonRuleSet',
+                        },
+                    },
+                    overrideAction: {
+                        none: {},
+                    },
+                    visibilityConfig: {
+                        sampledRequestsEnabled: true,
+                        cloudWatchMetricsEnabled: true,
+                        metricName: 'SSPWebACL-CRS',
+                    },
+                },
+            ],
+        });
+
+        const bucket = new Bucket(this, 'SSPBucketSPAId', {
+            bucketName: 'ssp-bucket-spa',
+            encryption: BucketEncryption.S3_MANAGED,
+            removalPolicy: RemovalPolicy.DESTROY
+        });
         
         const originAccessIdentity = new OriginAccessIdentity(this, 'SSPOriginAccessId');
 
@@ -23,14 +54,14 @@ export class SSPCloudfrontStack extends Stack {
             originConfigs: [
                 {
                     s3OriginSource: {
-                        s3BucketSource: props!.bucket,
+                        s3BucketSource: bucket,
                         originAccessIdentity: originAccessIdentity,
                     
                     },
                     behaviors: [{ isDefaultBehavior: true }],
                 },
             ],
-            webACLId: props!.waf.attrArn
+            webACLId: webAcl.attrArn
         });
 
         new CfnOutput(this, 'DistributionDomainName', { value: distribution.distributionDomainName });
